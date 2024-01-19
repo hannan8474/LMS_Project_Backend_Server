0a785721a2d08165d6ff4d33b1b8a1c99cca9a34
unknown <raoabdulhannan757@gmail.com>
2024-01-19 16:27:26 +0500

    Changes made, router added, somethings requires explanation like cors, required: true and router

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const connectToMongoDB = require('./config/db');
-const addStudentController = require('./controllers/student_controller/add_student_controller');
+const cors = require('cors');
+const router = require('./routes/student-routes/student-routes');
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const port = 3000;
 connectToMongoDB();
@@ -12,7 +14,7 @@ app.get('/', (req, res) => {
     res.send(`<h1> Hello, World! </h1>`);
 });
 
-app.post ('/add-student', addStudentController);
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`Server is at http://localhost:${port}`);
--- models/student_model/new_student.js
+++ models/student_model/new_student.js
@@ -1,17 +1,41 @@
-const mongoose = require ('mongoose');
-const { Schema } = mongoose;
+const mongoose = require('mongoose');
 
-const studentSchema = new Schema({
-    studentName: String, 
-    studentCnic: String,
-    email: String,
-    rollNumber: String,
-    batchNumber: String,
-    shift: String,
-    semester: String,
-    subjectNumbers: Number,
-    subjects: Array,
-}, {timestamps: true});
+const studentSchema = new mongoose.Schema({
+    studentName: {
+        type: String,
+        required: true,
+    },
+    studentCnic: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    rollNumber: {
+        type: String,
+        required: true,
+    },
+    batchNumber: {
+        type: String,
+        required: true,
+    },
+    shift: {
+        type: String,
+        required: true,
+    },
+    semester: {
+        type: String,
+        required: true,
+    },
+    subjectNumbers: {
+        type: Number,
+        required: true,
+    },
+    subjects: [Object],
+});
 
-const Student = mongoose.model ('student', studentSchema);
+const Student = mongoose.model('students', studentSchema);
+
 module.exports = Student;
